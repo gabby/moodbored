6c2e72ceb38e2d67e276016e59da72799c29a853
Gabrielle Sin <sin.gabrielle@gmail.com>
2017-09-24 18:37:09 -0400

    correctly link pin

--- client/components/Login.js
+++ client/components/Login.js
@@ -6,11 +6,11 @@ import store from '../store';
 const Login = () => (
   <div>
     <div>
-      <a href='/auth/pinterest'>
-        <Paper zDepth={3} circle={true} style={{overflow:'hidden', height:400, width:400, margin:20, display: 'flex', flexDirection:'row', justifyContent:'center'}}>
+      <Paper zDepth={3} circle={true} style={{overflow:'hidden', height:400, width:400, margin:20, display: 'flex', flexDirection:'row', justifyContent:'center'}}>
+        <a href='/auth/pinterest'>
           <img src="https://i.pinimg.com/originals/8f/82/c9/8f82c99bcd88ed685395a2b51174584b.jpg" style={{width:'100%', height:'auto'}}/>
-        </Paper>
-      </a>
+        </a>
+      </Paper>
     </div>
   </div>
 );
