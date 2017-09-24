7c39e7796362cfacb11157468b890b7ded338e9b
Gabrielle Sin <sin.gabrielle@gmail.com>
2017-09-24 18:31:46 -0400

    make photo clickable

--- client/components/RandomBoard.js
+++ client/components/RandomBoard.js
@@ -15,7 +15,6 @@ class RandomBoard extends Component {
   }
 
   componentWillMount(){
-    console.log('MADE IT INto MOUNT');
     let counter = 0; 
     while(counter < 6) {
       this.state.randomArr.push(Math.floor(Math.random()*this.props.userPins[0].length));
@@ -23,10 +22,8 @@ class RandomBoard extends Component {
     }
   }
   render(props){
-    console.log(this.state.randomArr);
     let pins = this.props.userPins[0];
     let randomIdx = this.state.randomArr
-    console.log(randomIdx)    
     return (
       <div>
        {
@@ -35,9 +32,11 @@ class RandomBoard extends Component {
             zDepth={1} 
             style={{overflow:'hidden', height:300, width:300, margin:20}}
             key={randomIdx.indexOf(idx)}>
-              <img 
-              src={pins[idx].image.original.url} 
-              style={{width:'100%', height:'auto'}}/>
+              <a href={pins[idx].link}>
+                <img 
+                src={pins[idx].image.original.url} 
+                style={{width:'100%', height:'auto'}}/>
+              </a>
           </Paper>
         }) 
        }
